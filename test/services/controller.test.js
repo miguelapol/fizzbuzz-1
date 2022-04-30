b45cab3a0829e85bcaf9d969cb58deecf082148e
miguelapol <apolonio1995@gmail.com>
2022-04-29 21:33:31 -0500

    hicimos un nuevo test donde compara una API dionde solo hay javascript

--- test/services/controller.test.js
+++ test/services/controller.test.js
@@ -1,7 +1,8 @@
 const controller=require("./../../lib/controllers/ExplorerController");
 describe("Pruebas de unidad", function() {
     test("controllers stacks", function() {
-        const out=[{githubUsername:"apol",score:1,mission:"node",stacks:["javascript","reasonML","elm"]}];
+        //aqui comparamos con una base test donde solo hay javascript
+        const out=require("../data/explorers.json")
         expect(controller.Stacks("javascript")).toEqual(out);
     });
 });
