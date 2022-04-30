6d4749d110cf586e959871be9b671b871ccb952a
miguelapol <apolonio1995@gmail.com>
2022-04-29 20:55:30 -0500

    haciendo pruebas del controller

---
+++ test/services/controller.test.js
@@ -0,0 +1,7 @@
+const controller=require("./../../lib/controllers/ExplorerController");
+describe("Pruebas de unidad", function() {
+    test("controllers stacks", function() {
+        const out=[{githubUsername:"apol",score:1,mission:"node",stacks:["javascript","reasonML","elm"]}];
+        expect(controller.Stacks("javascript")).toEqual(out);
+    })
+});
